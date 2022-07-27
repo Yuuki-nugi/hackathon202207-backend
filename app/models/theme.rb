66fb161abef7d151319d8dd418c380307f7612de
yuuki kunugi <kunuyuu3@gmail.com>
2022-07-28 03:47:29 +0900

    themeの更新のactioncableでのブロードキャスト

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,17 @@
 class Theme < ApplicationRecord
-    has_many :progress, dependent: :destroy
+    has_many :progress
 
     belongs_to :work
+
+    after_create do
+        ActionCable.server.broadcast("works_#{self.work_id}", theme: self, method: 'create')
+    end
+
+    after_update do
+        ActionCable.server.broadcast("works_#{self.work_id}", theme: self, method: 'update')
+    end
+
+    after_destroy do
+        ActionCable.server.broadcast("works_#{self.work_id}", theme: self, method: 'destroy')
+    end
 end
